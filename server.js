b34185c3f12994b79e7e88bf711d7149ec1afa0e
ankitdos12 <ankitdos14@gmail.com>
2025-08-07 13:19:57 +0530

    origin committed

--- server.js
+++ server.js
@@ -13,7 +13,17 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["http://localhost:5173","https://hdosexpence.spaadvisor.in"],
+    credentials: true,
+    exposedHeaders: ["set-cookie"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }
+));
 
 
 app.use("/api/auth", authRoutes);
