17555e449d52c127cd3e00fa6c895a6a82c53c9d
ankitdos12 <ankitdos14@gmail.com>
2025-08-21 15:41:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ app.use("/api/region", regionRoutes);
 app.use("/api/expense", expenseRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,  "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
